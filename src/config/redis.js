e664b4b561cb0693456f1dda7b07ad25c5cba530
Udinei <ukaliko@gmail.com>
2020-07-24 14:38:33 -0400

    ....referencia 07c495f

--- src/config/redis.js
+++ src/config/redis.js
@@ -2,24 +2,23 @@
     somente se estiver em ambiente de produção
     Nota: se o erro:  Ready check failed: NOAUTH Authentication required, for
     exibido no log, aguardar, pois o servidor heroku ira restartar a aplicação */
-const ambiente = () => {
-      if (process.env.NODE_ENV !== 'development')
-          return 'password: process.env.REDIS_PASSWORD';
-          return '';
+/*const ambiente = () => {
+  if (process.env.NODE_ENV !== 'development')
+    return 'password: process.env.REDIS_PASSWORD';
+    return '';
 
-}
+}*/
 
 
 // executa a funcao
-const password_ = ambiente();
-console.log("..... redis password : ", password_);
+//const password_ = ambiente();
+//console.log("..... redis password : ", password_);
 
 export default {
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
-  password_   // essa linha sera incluida somente quando subir para o heroku
-
-
+  password: process.env.REDIS_PASSWORD
+  //password_   // essa linha sera incluida somente quando subir para o heroku
 };
 
 
